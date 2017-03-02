d177eff8723ea93eb8dcd4a7a5f31b8c4f1b8370
DavidFrisch <dmf463@nyu.edu>
2017-03-02 17:14:36 -0500

    got camera working, and student spawn

---
+++ Proof of Concept Game/Assets/Scripts/HallwayRunManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HallwayRunManager : MonoBehaviour {
+
+    public float speed;
+    GameObject[] players;
+
+	// Use this for initialization
+	void Start () {
+
+        players = GameObject.FindGameObjectsWithTag("Player");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Camera.main.transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+        Vector3 playerMidpoint = ((players[1].transform.position - players[0].transform.position) * 0.5f) + players[0].transform.position;
+        playerMidpoint.z = -10f;
+        Camera.main.transform.position = playerMidpoint;
+        Camera.main.orthographicSize = Vector3.Distance(players[0].transform.position, players[1].transform.position);
+        //Debug.Log("Distance Between Players is equal to " + Vector3.Distance(players[0].transform.position, players[1].transform.position));
+    }
+}
---
+++ Proof of Concept Game/Assets/Scripts/StudentSpawner.cs
@@ -0,0 +1,61 @@
+﻿//HATY CATCHY by m@ boch - NYU GAMECENTER - Oct 2016
+
+using UnityEngine;
+using System.Collections;
+
+public class StudentSpawner : MonoBehaviour {
+    //public variables are accessible by other scripts, and are often set in the inspector
+    //they're great for tunable variables, like these, since we can edit them in play mode.
+    public GameObject studentPrefab1;
+    public GameObject studentPrefab2;
+    public float timeBetweenSpawns; //interval of time to wait between spawn in seconds
+
+    public float velocity;
+   
+    public float xSpawnPosMin; //left most spawn point
+    public float xSpawnPosMax; //right most spawn point
+    public float ySpawnPos; //height of spawn
+
+    //private variables are more like the global variables in Phaser, and 
+    //they can't be accessed by other scripts
+    public float timeUntilSpawn;
+
+    public void Start()
+    {
+        timeUntilSpawn = 1;
+    }
+
+    public void Update()
+    {
+        //Time.delaTime is how much time has occured since the last update. 
+        //We subtract it from time until spawn every frame
+        timeUntilSpawn -= Time.deltaTime;
+        //Once timeUntilSpawn is less than 0, we spawn a new hat
+        if (timeUntilSpawn <= 0)
+        {
+            SpawnStudent();
+            //then we reset timeUntilSpawn to the timeBetweenSpawns & start all over again
+            timeUntilSpawn = timeBetweenSpawns;
+        }
+    }
+
+    private void SpawnStudent()
+    {
+        //Generate a new spawn position. 
+        //For the first value of this Vector3 (x) we use Random.Range to get a position between our min & max X values
+        //The second (y) is just the height we spawn at
+        //The third (z) is the depth, which is set to 0 as we're in 2D
+        Vector3 newPos = new Vector3(Random.Range(xSpawnPosMin, xSpawnPosMax), ySpawnPos, 0);
+        //Instantiate creates a new object from a prefab. It needs the prefab, the position, and the rotation as arguements
+        //newPos is the position we made, Quaternion.identity just means that we're not rotating the sprite
+        GameObject student1 =  Instantiate(studentPrefab1, newPos, Quaternion.identity);
+        student1.GetComponent<Rigidbody2D>().velocity = new Vector3(velocity, 0, 0);
+
+        Vector3 newPos2 = new Vector3(Random.Range(xSpawnPosMin, xSpawnPosMax), ySpawnPos, 0);
+        GameObject student2 = Instantiate(studentPrefab2, newPos2, Quaternion.identity);
+        student2.GetComponent<Rigidbody2D>().velocity = new Vector3(velocity, 0, 0);
+    }
+
+ 
+
+}
