d73e3079ada10cc684bd2d78389cd8945a28fd80
DavidFrisch <dmf463@nyu.edu>
2017-03-03 14:11:20 -0500

    fixed the camera and added a way to destroy students so they don't clutter up the system

--- Proof of Concept Game/Assets/Scripts/HallwayRunManager.cs
+++ Proof of Concept Game/Assets/Scripts/HallwayRunManager.cs
@@ -22,6 +22,11 @@ public class HallwayRunManager : MonoBehaviour {
             other.GetComponent<Rigidbody2D>().velocity = new Vector3(0, downVelocity, 0);
         }
     }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(other.gameObject);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -32,9 +37,9 @@ public class HallwayRunManager : MonoBehaviour {
         playerMidpoint.z = -10f;
         Camera.main.transform.position = playerMidpoint;
         Camera.main.orthographicSize = Vector3.Distance(players[0].transform.position, players[1].transform.position);
-        if(Camera.main.orthographicSize < 5)
+        if(Camera.main.orthographicSize < 2)
         {
-            Camera.main.orthographicSize = 5;
+            Camera.main.orthographicSize = 2;
         }
         //Debug.Log("Distance Between Players is equal to " + Vector3.Distance(players[0].transform.position, players[1].transform.position));
     }
